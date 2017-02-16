61bf47151c8d30b7518c26046743e5b4cc3283ed
itplamen@gmail.com <Common1ypeSys1em>
2017-02-16 03:14:58 +0200

    Added flight legs services

---
+++ Balkan Air/Services/BalkanAir.Services.Data/Contracts/IFlightLegsServices.cs
@@ -0,0 +1,19 @@
+﻿namespace BalkanAir.Services.Data.Contracts
+{
+    using System.Linq;
+
+    using BalkanAir.Data.Models;
+    
+    public interface IFlightLegsServices
+    {
+        int AddFlightLeg(FlightLeg flightLeg);
+
+        IQueryable<FlightLeg> GetAll();
+
+        FlightLeg GetFlightLeg(int id);
+
+        FlightLeg UpdateFlightLeg(int id, FlightLeg flightLeg);
+
+        FlightLeg DeleteFlightLeg(int id);
+    }
+}
---
+++ Balkan Air/Services/BalkanAir.Services.Data/FlightLegsServices.cs
@@ -0,0 +1,97 @@
+﻿namespace BalkanAir.Services.Data
+{
+    using System;
+    using System.Linq;
+
+    using BalkanAir.Data.Models;
+    using BalkanAir.Data.Repositories.Contracts;
+    using Common;
+    using Contracts;
+
+    public class FlightLegsServices : IFlightLegsServices
+    {
+        private readonly IRepository<FlightLeg> flightLegs;
+
+        public FlightLegsServices(IRepository<FlightLeg> flightLegs)
+        {
+            this.flightLegs = flightLegs;
+        }
+
+        public int AddFlightLeg(FlightLeg flightLeg)
+        {
+            if (flightLeg == null)
+            {
+                throw new ArgumentNullException(ErrorMessages.ENTITY_CANNOT_BE_NULL);
+            }
+
+            this.flightLegs.Add(flightLeg);
+            this.flightLegs.SaveChanges();
+
+            return flightLeg.Id;
+        }
+
+        public IQueryable<FlightLeg> GetAll()
+        {
+            return this.flightLegs.All();
+        }
+
+        public FlightLeg GetFlightLeg(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(ErrorMessages.INVALID_ID);
+            }
+
+            return this.flightLegs.GetById(id);
+        }
+
+        public FlightLeg UpdateFlightLeg(int id, FlightLeg flightLeg)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(ErrorMessages.INVALID_ID);
+            }
+
+            if (flightLeg == null)
+            {
+                throw new ArgumentNullException(ErrorMessages.ENTITY_CANNOT_BE_NULL);
+            }
+
+            var flightLegToUpdate = this.flightLegs.GetById(id);
+
+            if (flightLegToUpdate != null)
+            {
+                flightLegToUpdate.DepartureAirportId = flightLeg.DepartureAirportId;
+                flightLegToUpdate.ScheduledDepartureTime = flightLeg.ScheduledDepartureTime;
+                flightLegToUpdate.ArrivalAirportId = flightLeg.ArrivalAirportId;
+                flightLegToUpdate.ScheduledArrivalTime = flightLeg.ScheduledArrivalTime;
+                flightLegToUpdate.IsDeleted = flightLeg.IsDeleted;
+                flightLegToUpdate.FlightId = flightLeg.FlightId;
+                flightLegToUpdate.RouteId = flightLeg.RouteId;
+
+                this.flightLegs.SaveChanges();
+            }
+
+            return flightLegToUpdate;
+        }
+
+        public FlightLeg DeleteFlightLeg(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(ErrorMessages.INVALID_ID);
+            }
+
+            var flightLegToDelete = this.flightLegs.GetById(id);
+
+            if (flightLegToDelete != null)
+            {
+                flightLegToDelete.IsDeleted = true;
+
+                this.flightLegs.SaveChanges();
+            }
+
+            return flightLegToDelete;
+        }
+    }
+}
