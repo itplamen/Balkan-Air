cff073d98c214177983fb923a64912960e2314f3
itplamen@gmail.com <Common1ypeSys1em>
2017-02-16 03:24:10 +0200

    Added leg instances services

---
+++ Balkan Air/Services/BalkanAir.Services.Data/Contracts/ILegInstancesServices.cs
@@ -0,0 +1,19 @@
+﻿namespace BalkanAir.Services.Data.Contracts
+{
+    using System.Linq;
+
+    using BalkanAir.Data.Models;
+
+    public interface ILegInstancesServices
+    {
+        int AddLegInstance(LegInstance legInstance);
+
+        IQueryable<LegInstance> GetAll();
+
+        LegInstance GetLegInstance(int id);
+
+        LegInstance UpdateLegInstance(int id, LegInstance legInstance);
+
+        LegInstance DeleteLegInstance(int id);
+    }
+}
---
+++ Balkan Air/Services/BalkanAir.Services.Data/LegInstancesServices.cs
@@ -0,0 +1,97 @@
+﻿namespace BalkanAir.Services.Data
+{
+    using System;
+    using System.Linq;
+
+    using BalkanAir.Data.Models;
+    using BalkanAir.Data.Repositories.Contracts;
+    using Common;
+    using Contracts;
+
+    public class LegInstancesServices : ILegInstancesServices
+    {
+        private readonly IRepository<LegInstance> legInstances;
+
+        public LegInstancesServices(IRepository<LegInstance> legInstances)
+        {
+            this.legInstances = legInstances;
+        }
+
+        public int AddLegInstance(LegInstance legInstance)
+        {
+            if (legInstance == null)
+            {
+                throw new ArgumentNullException(ErrorMessages.ENTITY_CANNOT_BE_NULL);
+            }
+
+            this.legInstances.Add(legInstance);
+            this.legInstances.SaveChanges();
+
+            return legInstance.Id;
+        }
+
+        public IQueryable<LegInstance> GetAll()
+        {
+            return this.legInstances.All();
+        }
+
+        public LegInstance GetLegInstance(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(ErrorMessages.INVALID_ID);
+            }
+
+            return this.legInstances.GetById(id);
+        }
+
+        public LegInstance UpdateLegInstance(int id, LegInstance legInstance)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(ErrorMessages.INVALID_ID);
+            }
+
+            if (legInstance == null)
+            {
+                throw new ArgumentNullException(ErrorMessages.ENTITY_CANNOT_BE_NULL);
+            }
+
+            var legInstanceToUpdate = this.legInstances.GetById(id);
+
+            if (legInstanceToUpdate != null)
+            {
+                legInstanceToUpdate.DateOfTravel = legInstance.DateOfTravel;
+                legInstanceToUpdate.DepartureTime = legInstance.DepartureTime;
+                legInstanceToUpdate.ArrivalTime = legInstance.ArrivalTime;
+                legInstanceToUpdate.IsDeleted = legInstance.IsDeleted;
+                legInstanceToUpdate.FlightLegId = legInstance.FlightLegId;
+                legInstanceToUpdate.FlightStatusId = legInstance.FlightStatusId;
+                legInstanceToUpdate.AircraftId = legInstance.AircraftId;
+
+                this.legInstances.SaveChanges();
+            }
+
+            return legInstanceToUpdate;
+        }
+
+        public LegInstance DeleteLegInstance(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(ErrorMessages.INVALID_ID);
+            }
+
+            var legInstanceToDelete = this.legInstances.GetById(id);
+
+            if (legInstanceToDelete != null)
+            {
+                legInstanceToDelete.IsDeleted = true;
+
+                this.legInstances.SaveChanges();
+            }
+
+            return legInstanceToDelete;
+        }
+    }
+}
