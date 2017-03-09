de00e5dc903ac6177cc9bb6369948ab971d3e191
itplamen@gmail.com <Common1ypeSys1em>
2017-03-10 00:12:50 +0200

    Updated AdministrationHome.aspx.cs to show all administrators.

--- Balkan Air/Web/BalkanAir.Web/Administration/AdministrationHome.aspx.cs
+++ Balkan Air/Web/BalkanAir.Web/Administration/AdministrationHome.aspx.cs
@@ -3,44 +3,34 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Web;
     using System.Web.UI;
-    using System.Web.UI.WebControls;
 
     using Ninject;
 
- 
+    using Data.Common;
     using Data.Models;
-    using BalkanAir.Services.Data.Contracts;
+    using Services.Data.Contracts;
 
     public partial class AdministrationHome : Page
     {
         [Inject]
         public IUsersServices UsersServices { get; set; }
 
-        //[Inject]
-        //public IUserRolesServices UserRolesServices { get; set; }
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-        }
+        [Inject]
+        public IUserRolesServices UserRolesServices { get; set; }
 
         public IEnumerable<User> AdministratorsRepeater_GetData()
         {
-            //var administratorRole = this.UserRolesServices.GetAll()
-            //    .FirstOrDefault(r => r.Name.ToLower().Equals(GlobalConstants.ADMINISTRATOR_ROLE));
+            var administratorRole = this.UserRolesServices.GetAll()
+                .FirstOrDefault(r => r.Name == UserRolesConstants.ADMINISTRATOR_ROLE);
 
-            //if (administratorRole == null)
-            //{
-            //    return null;
-            //}
+            return this.UsersServices.GetAll()
+                .Where(u => u.Roles.Select(r => r.RoleId).Contains(administratorRole.Id))
+                .ToList();
+        }
 
-
-            //return this.UsersServices.GetAll()
-            //    .Where(u => u.Roles.Select(s => s.RoleId).Contains(administratorRole.Id))
-            //    .ToList(); ;
-
-            return null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
         }
     }
 }
