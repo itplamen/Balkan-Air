c8c958cd7f1a27132ee221ce31540d8454730fa7
itplamen@gmail.com <Common1ypeSys1em>
2017-03-18 19:06:56 +0200

    Added FlightNumbersController and related data transfer models.

---
+++ Balkan Air/Web/BalkanAir.Web/Areas/Api/Controllers/FlightNumbersController.cs
@@ -0,0 +1,119 @@
+﻿namespace BalkanAir.Web.Areas.Api.Controllers
+{
+    using Services.Data.Contracts;
+    using System.Linq;
+    using System.Web.Http;
+    using System.Web.Http.Cors;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+
+    using Ninject;
+
+    using Data.Common;
+    using Data.Helper;
+    using Data.Models;
+    using Models.FlightNumbers;
+    using Services.Common;
+
+    /// <summary>
+    /// This controller must be accessible only from the Administrator.
+    /// </summary>
+
+    [EnableCors("*", "*", "*")]
+    [Authorize(Roles = UserRolesConstants.ADMINISTRATOR_ROLE)]
+    public class FlightNumbersController : ApiController
+    {
+        private readonly IFlightsServices flightsServices;
+
+        [Inject]
+        public INumberGenerator NumberGenerator { get; set; }
+
+        public FlightNumbersController(IFlightsServices flightsServices)
+        {
+            this.flightsServices = flightsServices;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create()
+        {
+            var flightNumberToAdd = new Flight() { Number = this.NumberGenerator.GetUniqueFlightNumber() };
+            var addedFlightNumberId = this.flightsServices.AddFlight(flightNumberToAdd);
+
+            return this.Ok(addedFlightNumberId);
+        }
+
+        [HttpGet]
+        public IHttpActionResult All()
+        {
+            var flightNumbers = this.flightsServices.GetAll()
+                .OrderBy(f => f.Number)
+                .ProjectTo<FlightNumberResponseModel>()
+                .ToList();
+
+            return this.Ok(flightNumbers);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            if (id <= 0)
+            {
+                return this.BadRequest(ErrorMessages.INVALID_ID);
+            }
+
+            var flightNumber = this.flightsServices.GetAll()
+                .ProjectTo<FlightNumberResponseModel>()
+                .FirstOrDefault(f => f.Id == id);
+
+            if (flightNumber == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(flightNumber);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(int id, UpdateFlightNumberRequestModel flightNumber)
+        {
+            if (id <= 0)
+            {
+                return this.BadRequest(ErrorMessages.INVALID_ID);
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var flightNumberToUpdate = Mapper.Map<Flight>(flightNumber);
+            var updatedFlightNumber = this.flightsServices.UpdateFlight(id, flightNumberToUpdate);
+
+            if (updatedFlightNumber == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(updatedFlightNumber.Id);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return this.BadRequest(ErrorMessages.INVALID_ID);
+            }
+
+            var deletedFlightNumber = this.flightsServices.DeleteFlight(id);
+
+            if (deletedFlightNumber == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(deletedFlightNumber.Id);
+        }    
+    }
+}
---
+++ Balkan Air/Web/BalkanAir.Web/Areas/Api/Models/FlightNumbers/UpdateFlightNumberRequestModel.cs
@@ -0,0 +1,23 @@
+﻿namespace BalkanAir.Web.Areas.Api.Models.FlightNumbers
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using Data.Common;
+    using Data.Models;
+    using Infrastructure.Mapping;
+    
+    public class UpdateFlightNumberRequestModel : IMapFrom<Flight>
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [StringLength(ValidationConstants.FLIGHT_NUMBER_LENGTH)]
+        public string Number { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+    }
+}
